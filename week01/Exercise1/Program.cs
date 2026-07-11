ad8d601d911f71dbf7654fa8f37376872b2ce119
unknown <leonardotzepchox@gmail.com>
2026-07-11 13:51:04 -0600

    I added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the used for theier name.
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 

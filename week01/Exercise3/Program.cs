51fcb84bbcb3c0db65e298891f5ad83af7ea3f63
unknown <leonardotzepchox@gmail.com>
2026-07-13 10:44:22 -0600

    Code for play guess game written

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            int magicNumb = randomGenerator.Next(1,100);
+            int guess = -1;
+
+            while (guess != magicNumb)
+            {
+                Console.Write("What is the number you would like to guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumb > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumb < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guess it! ");
+                    Console.WriteLine("Would you like to play again? yes/no ");
+                    response = Console.ReadLine();
+                }
+
+            }
+        }
+
     }
 }

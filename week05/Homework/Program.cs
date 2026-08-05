759ccf5245c342c3d6b520e7f807faae29fd47bd
unknown <leonardotzepchox@gmail.com>
2026-08-04 22:20:11 -0600

    Program Class code written

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Diego", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Moroni", "Fractions", "7.3", "9-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Diego Tzep", "Guatemalan History", "The good doctor");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
+

a44371bbc6d018d5fa64a79b1a2e18e5a6cf735b
unknown <leonardotzepchox@gmail.com>
2026-08-10 15:26:42 -0600

    Class Square code written

---
+++ week06/Shapes/Square.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+
+class Square : Shape
+{
+    private double _side;
+
+
+    public Square(string color, double side)
+        :base (color)
+    {
+        _side = side;
+    }
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}

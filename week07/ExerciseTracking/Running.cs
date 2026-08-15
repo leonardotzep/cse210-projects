dd99274583057e4514688216549a9548ca56aa6c
unknown <leonardotzepchox@gmail.com>
2026-08-15 09:55:15 -0600

    Class Running code written

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Running : Activity
+{
+    private double _distanceDone;
+
+
+    public Running(double distanceDone, DateTime date, double duration)
+    {
+        _distanceDone = distanceDone;
+        _date = date;
+        _duration = duration;
+    }
+
+
+    public override double GetDistance()
+    {
+        return _distanceDone;
+    }
+
+
+    public override double GetSpeed()
+    {
+        return _distanceDone / (_duration / 60);
+    }
+
+
+    public override double GetPace()
+    {
+        return _duration / _distanceDone;
+    }
+}

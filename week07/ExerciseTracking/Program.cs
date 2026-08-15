cb375393149f7dc4fe0a23d9b89f84c00e6b7402
unknown <leonardotzepchox@gmail.com>
2026-08-15 09:54:32 -0600

    Class Program code written

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Diagnostics;
+using System.Dynamic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(3.0, new DateTime(2022, 11, 3), 30));
+        activities.Add(new StationaryBicycles(10.0, new DateTime(2022, 11, 3), 25));
+        activities.Add(new Swimming(40, new DateTime(2022, 11, 3), 35));
+
+        foreach(Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
